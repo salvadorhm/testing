d168fcd88c7ef98e51262618b6e17ce473675a66
Salvador HM <salvadorhm@gmail.com>
2023-02-09 09:09:55 -0600

    CREATED version 3

--- mayor.py
+++ mayor.py
@@ -1,18 +1,13 @@
-def mayor(numero1:int, numero2:int)->int | None:
-    result = 0
-    if numero1 > numero2:
-        result = numero1
-    elif numero2 > numero1:
-        result = numero2
-    else:
-        result = None
-    return result
+class Mayor:
+    def __init__(self) -> None:
+        pass
 
-def test_mayor1():
-    assert mayor(2,1) == 2
-
-def test_mayor2():
-    assert mayor(1,2) == 2
-
-def test_mayor3():
-    assert mayor(2,2) == None
+    def mayor(self, numero1:int, numero2:int)->int | None:
+        result = 0
+        if numero1 > numero2:
+            result = numero1
+        elif numero2 > numero1:
+            result = numero2
+        else:
+            result = None
+        return result
---
+++ test/test_mayor.py
@@ -0,0 +1,12 @@
+from mayor import Mayor as m
+
+class TestMayor:
+
+    def test_mayor1(self):
+        assert m.mayor(self,2,1) == 2 # type: ignore
+
+    def test_mayor2(self):
+        assert m.mayor(self,1,2) == 2 # type: ignore
+
+    def test_mayor3(self):
+        assert m.mayor(self,2,2) == None # type: ignore
