0638a4dded604256002858ea3f367bdfbe97ec98
Salvador HM <salvadorhm@gmail.com>
2023-02-09 08:32:36 -0600

    CREATED version 2

--- mayor.py
+++ mayor.py
@@ -1,11 +1,18 @@
 def mayor(numero1:int, numero2:int)->int | None:
-    result = None
+    result = 0
     if numero1 > numero2:
         result = numero1
     elif numero2 > numero1:
         result = numero2
+    else:
+        result = None
     return result
 
-print(mayor(2,1)) # 2
-print(mayor(1,2)) # 2
-print(mayor(2,2)) # None
+def test_mayor1():
+    assert mayor(2,1) == 2
+
+def test_mayor2():
+    assert mayor(1,2) == 2
+
+def test_mayor3():
+    assert mayor(2,2) == None
